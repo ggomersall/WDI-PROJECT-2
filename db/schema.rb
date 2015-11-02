804dfd23878ab4486e5d0450fc1008d602781268
ggomersall <ggomersall@gmail.com>
2015-11-02 09:34:56 +0000

    ran rake db:migrate for addresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101160454) do
+ActiveRecord::Schema.define(version: 20151102092926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "telephone"
+    t.string   "address_line_1"
+    t.string   "address_line_2"
+    t.string   "city"
+    t.string   "county"
+    t.string   "post_code"
+    t.string   "country"
+    t.text     "delivery_notes"
+    t.string   "company_name"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "addresses", ["user_id"], name: "index_addresses_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -37,4 +54,5 @@ ActiveRecord::Schema.define(version: 20151101160454) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "addresses", "users"
 end
